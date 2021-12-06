e651a16ffe6beaef1e7525ea0d0c0f673efbddf8
Elijah Okello <elijahokello90@gmail.com>
2021-12-06 10:17:47 +0300

    My initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author elijahokello
+ */
+public class Main {
+    public static void main(String[] args){
+        for(int i = 40;i<=70;i++){
+            if(i%2 == 0){
+                System.out.println(i*2);
+            }
+        }
+    }
+}
